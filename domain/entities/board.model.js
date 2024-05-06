479d9cbc743816f6ca4733530abfe5915b5d1712
Alexis Aravena <aaravena@inge.cl>
2024-05-05 20:48:35 -0400

    Modelo inicial de tablero

--- domain/entities/board.model.js
+++ domain/entities/board.model.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const boardSchema = mongoose.Schema(
     {
-        
+        name: {
+            type: String,
+            required: [true, "Please enter board name"]
+        },
+    },
+    {
+        timestamps: true
     }
 );
+const Board = mongoose.model("Board", boardSchema);
+module.exports = Board;
